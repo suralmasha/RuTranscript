dfae660359d72241046337abbfdcb8da255d2abd
suralmasha <65304290+suralmasha@users.noreply.github.com>
2022-05-13 22:29:59 +0300

    Update ru_transcript.py

--- ru_transcript.py
+++ ru_transcript.py
@@ -5,7 +5,7 @@ import spacy
 from nltk import Tree
 from stressrnn import StressRNN
 import epitran
-from Sounds import Sounds
+from sounds_m import Sounds
 
 with open('Data/alphabet.txt', 'r') as f:
     alphabet = f.read()
