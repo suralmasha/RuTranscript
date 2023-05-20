f38413990f24f7c5b2e5b8b7296a9b0b6d345cbc
suralmasha <sashabadasyan@icloud.com>
2023-05-20 13:56:33 +0300

    package_data

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@
 # Author: suralmasha - Badasyan Alexandra
 
 from setuptools import setup, find_packages
-# from shutil import copytree, copy, rmtree
-# from os import path
-# import glob
-import os
 
 
 def requirements():
@@ -25,21 +21,12 @@ def requirements():
         return deps
 
 
-def add_files(path):
-    added_files = []
-    for dir_name, _, files in os.walk(path):
-        for filename in files:
-            added_files.append(os.path.join(dir_name, filename))
-    return added_files
-
-
 def readme():
     with open('README.md', 'r', encoding='utf-8') as f_readme:
         return f_readme.read()
 
 
 if __name__ == "__main__":
-    # sources_dir = 'ru_transcript'
     setup(
         name='ru_transcript',
         # version='0.1.0',
@@ -55,9 +42,8 @@ if __name__ == "__main__":
         keywords='nlp russian transcription phonetics linguistic',
         author_email='sashabadasyan@icloud.com',
 
-        # packages=['ru_transcript'],
-        # package_data={'ru_transcript': add_files(sources_dir)},
         packages=find_packages(exclude=['tests', 'example.py', 'jpt_example.ipynb']),
+        package_data={'ru_transcript': ['data/*']},
         include_package_data=True,
         install_requires=requirements(),
         zip_safe=False
