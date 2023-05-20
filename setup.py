323888657774c4df3ff9b120dfe6f6644a23e3b7
suralmasha <sashabadasyan@icloud.com>
2023-05-20 13:52:40 +0300

    package_data

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@
 # -*- coding: utf-8 -*-
 # Author: suralmasha - Badasyan Alexandra
 
-from setuptools import setup  # , find_packages
+from setuptools import setup, find_packages
 # from shutil import copytree, copy, rmtree
 # from os import path
+# import glob
 import os
-import glob
 
 
 def requirements():
     with open('requirements.txt', 'r', encoding='utf-8') as f_requirements:
         deps = []
         while True:
-            l = f_requirements.readline()
-            if l == '':
+            row = f_requirements.readline()
+            if row == '':
                 break
-            elif l.startswith('--'):
+            elif row.startswith('--'):
                 continue
-            elif l.startswith('-e'):
-                url, name = l.split('#egg=')
-                l = name.replace('\n', '') + ' @' + url.replace('-e', '')
-            deps.append(l)
+            elif row.startswith('-e'):
+                url, name = row.split('#egg=')
+                row = name.replace('\n', '') + ' @' + url.replace('-e', '')
+            deps.append(row)
         return deps
 
 
@@ -39,19 +39,7 @@ def readme():
 
 
 if __name__ == "__main__":
-    sources_dir = 'ru_transcript'
-    # temp_dir = 'temp'
-
-    # Prepare temp folders
-    # rmtree(temp_dir, ignore_errors=True)
-    # copytree(path.join(sources_dir, 'data'), path.join(temp_dir, 'data'), copy_function=copy)
-    # copytree(path.join(sources_dir, 'tools'), path.join(temp_dir, 'tools'), copy_function=copy)
-    # copy(path.join(sources_dir, 'ru_transcript.py'), path.join(temp_dir, 'ru_transcript.py'))
-    # f = open(path.join(temp_dir, '__init__.py'), 'w')
-    # f.write('from RuTranscript.RuTranscript import RuTranscript'
-    #        '\nfrom RuTranscript.tools.allophones_tools import get_allophone_info')
-    # f.close()
-
+    # sources_dir = 'ru_transcript'
     setup(
         name='ru_transcript',
         # version='0.1.0',
@@ -67,8 +55,9 @@ if __name__ == "__main__":
         keywords='nlp russian transcription phonetics linguistic',
         author_email='sashabadasyan@icloud.com',
 
-        packages=['ru_transcript'],
-        package_data={'ru_transcript': add_files(sources_dir)},
+        # packages=['ru_transcript'],
+        # package_data={'ru_transcript': add_files(sources_dir)},
+        packages=find_packages(exclude=['tests', 'example.py', 'jpt_example.ipynb']),
         include_package_data=True,
         install_requires=requirements(),
         zip_safe=False
