beee486397711fc11e55185a627b5f835021db4c
alexandra badasian <abadasian@neuro.net>
2023-12-07 13:07:09 +0300

    added __all__ to __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,9 @@
 from .RuTranscript import RuTranscript
 from .tools.allophones_tools import get_allophone_info
+from .tools.main_tools import text_norm_tok
+
+__all__ = [
+    'RuTranscript',
+    'get_allophone_info',
+    'text_norm_tok'
+]
